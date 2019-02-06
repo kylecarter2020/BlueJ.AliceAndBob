874687bbc979cda14477928a017958a9d989533f
Kyle Carter <kylecarter@zipcoders-MBP-9.T-mobile.com>
2019-02-06 00:29:06 -0500

    created alice and bob name checker

--- Main.java
+++ Main.java
@@ -1,9 +1,28 @@
+import java.util.*;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
-
+    public static void main(String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        String nm = "";
+        
+        System.out.println("Hey,whats your name?");
+        nm = sc.nextLine();
+        
+        if(nm.equals("Bob"))
+        {
+            System.out.println("Yo Bobby! My Man! Wassup?!");
+        }
+        else if(nm.equals("Alice"))
+        {
+            System.out.println("Hey Alice! How's Wonderland girl?!");
+        }
+        else
+        {
+             System.out.println("Bye Felicia!");
+        }
     }
 }
